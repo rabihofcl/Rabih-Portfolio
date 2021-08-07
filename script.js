31c2556bb45a007a0f547a3bdc259639b0b19f80
rabihofcl <rabihofcl@gmail.com>
2021-08-07 12:32:01 +0530

    body collapse modified

--- script.js
+++ script.js
@@ -18,10 +18,6 @@ $(document).ready(function () {
         return this.optional(element) || value == value.match(/\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i);
     });
 
-    $('.navbar-collapse a').click(function(){
-        $(".navbar-collapse").collapse('hide');
-    });
-
     $('body').click(function(){
         $(".navbar-collapse").collapse('hide');
     });
